bf5aea3f7cbc88b5e9aeaff67a9b10205558b8a2
Bo Lee <libcell@cqu.edu.cn>
2023-08-02 13:42:25 +0800

    update

--- R/Day07-reading and writing.R
+++ R/Day07-reading and writing.R
@@ -38,6 +38,9 @@ csv4.data <- read.delim("clipboard", header = TRUE)
 dim(csv4.data)
 csv4.data
 
+csv5.data <- read.table(file = file.choose(), header = TRUE)
+class(csv5.data)
+
 # install.packages("openxlsx")
 library(openxlsx)
 
@@ -50,17 +53,67 @@ write.xlsx(df, "E:/testtable.xlsx")
 
 write.table(df, "E:/newtable2.txt", row.names = FALSE)
 write.csv(df, "E:/newtable2.csv", row.names = FALSE)
+
+# 
+install.packages("xlsx") # JRE & JDK
+
+library(xlsx)
+help(package = "xlsx")
+
 ### End of Step-01.
 ### ****************************************************************************
 
 ### ****************************************************************************
 ### Step-2. Reading XML Files
 
+pkgs <- c("XML", "RCurl")
+install.packages(pkgs)
+
+library(XML)
+library(RCurl)
+
+url2 <- "http://www.letpub.com.cn/index.php?journalid=6775&page=journalapp&view=detail"
+con2 <- getURL(url2)
+print(con2)
+sink("D:/record.txt")
+print(con2)
+sink()
+
+tables <- readHTMLTable(con2)
+tables
+
+str(tables)
+
+tables[[1]]
+tables[[4]]
+tables[[5]]
+
 ### End of Step-02.
 ### ****************************************************************************
 
 ### ****************************************************************************
 ### Step-3. Reading JSON Files
+
+library(rvest)
+page <- read_html(url)
+table_data <- page %>%
+  html_nodes("table") %>%
+  html_table(fill = TRUE)
+
+for (i in seq_along(table_data)) {
+  cat(paste0("Table ", i, ":\n"))
+  print(table_data[[i]])
+  cat("\n")
+}
+
+# 提取其他内容
+other_data <- page %>%
+  html_nodes("p") %>%
+  html_text()
+
+# 输出其他内容
+cat("其他内容:\n")
+cat(other_data)
 
 # install.paclages("rjson")
 
