6c7b4fa24854dc5a85c693e931ec79d3c6bf1779
Bo Lee <libcell@cqu.edu.cn>
2023-08-26 16:49:24 +0800

    Create Day15-Advanced drawing-3.R

---
+++ R/Day15-Advanced drawing-3.R
@@ -0,0 +1,115 @@
+################################################################################
+#    &&&....&&&    % BioMed 2023: Learning R Course in Summer of 2023          #            #
+#  &&&&&&..&&&&&&  % Teacher: Bo Li, Mingwei Liu                               #
+#  &&&&&&&&&&&&&&  % Date: Aug. 26th, 2023                                     #
+#   &&&&&&&&&&&&   %                                                           #
+#     &&&&&&&&     % Environment: R version 4.2.3;                             #
+#       &&&&       % Platform: x86_64-w64-mingw32/x64 (64-bit)                 #
+#        &         %                                                           #
+################################################################################
+
+################################################################################
+### code chunk number 15: Advanced drawing in R.
+################################################################################
+
+### ****************************************************************************
+### Step-01. About working directory in R. 
+
+# For windows, work directory. 
+
+pri.dir <- getwd()
+
+# setwd("D:/00-GitHub/LRC2.0/tmp/")
+
+### End of Step-01.
+### ****************************************************************************
+
+### ****************************************************************************
+### Step-02. Case-1. 
+
+library(ggplot2)
+p <- ggplot(data=mpg, mapping=aes(x=cty, y=hwy)) #第一层，画布
+p + geom_point() #第二层，画散点图
+
+p <-ggplot(mpg, aes(x=cty, y=hwy, color=factor(year)))
+p + geom_point()
+
+p <- ggplot(mpg, aes(x=cty, y=hwy, color=factor(year), size=displ))
+p + geom_point()
+
+p <- ggplot(data=mpg, mapping=aes(x=cty, y=hwy))
+p <- p + geom_point(aes(colour=factor(year),size=displ))
+p <- p + stat_smooth()
+p + scale_color_manual(values =c('blue2','red4'))  #增加标度
+
+
+p <- ggplot(data=mpg, mapping=aes(x=cty, y=hwy))
+p <- p + geom_point(aes(colour=class,size=displ))
+p <- p+ stat_smooth()
+p <- p + geom_point(aes(colour=factor(year),size=displ))
+p <- p + scale_size_continuous(range = c(4, 10))  #增加标度
+p + facet_wrap(~ year, ncol=1)     #分面
+
+
+# Case-5
+
+p <- ggplot(mpg, aes(x=cty, y=hwy))
+p <- p + geom_point(aes(colour=class,size=displ))
+p <- p + stat_smooth()
+p <- p + scale_size_continuous(range = c(2, 5))
+p <- p + facet_wrap(~ year,ncol=1)
+p <- p + ggtitle('汽车油耗与型号')                   #添加标题
+p <- p + labs(y='每加仑高速公路行驶距离',            #坐标轴修饰
+              x='每加仑城市公路行驶距离')
+p <- p + guides(size=guide_legend(title='排量'),     #修改图例
+               colour = guide_legend(title='车型',override.aes=list (size =5)))
+p
+
+# Case-6 
+
+class2 <- mpg$class                                         # 取出一列
+class2 <- reorder(class2,class2,length)            # 排序
+mpg$class2 <- class2                                     # 对mpg增加一列
+p  <-  ggplot(mpg, aes(x=class2))                    # 画布
+p  +  geom_bar(aes(fill=class2))                      # 绘制条形图
+
+
+# Case-7 
+
+p <- ggplot(mpg, aes(class2,fill=factor(year)))   #分组填充
+p  + geom_bar(position='identity', alpha=0.5)  
+
+p <- ggplot(mpg, aes(class2,fill=factor(year)))   #分组填充
+p  + geom_bar(position='dodge',alpha=0.5)   #
+
+p <- ggplot(mpg, aes(class2,fill=factor(year)))   #分组填充
+p + geom_bar(position='stack',alpha=0.5)   #叠加条形图
+
+p <- ggplot(mpg, aes(class2,fill=factor(year)))   #分组填充
+p  + geom_bar(position='fill',alpha=0.5)   
+
+# rChart package
+
+require(devtools)
+install_github('ramnathv/rCharts')
+
+require(rCharts)
+
+## Example 1 Facetted Scatterplot
+names(iris) = gsub("\\.", "", names(iris))
+rPlot(SepalLength ~ SepalWidth | Species, data = iris, color = 'Species', type = 'point')
+
+## Example 2 Facetted Barplot
+hair_eye = as.data.frame(HairEyeColor)
+rPlot(Freq ~ Hair | Eye, color = 'Eye', data = hair_eye, type = 'bar')
+
+r1 <- rPlot(mpg ~ wt | am + vs, data = mtcars, type = 'point', color = 'gear')
+r1
+### End of Step-01.
+### ****************************************************************************
+
+
+################################################################################
+### End of chunk-15.
+################################################################################
+
